cf5a2ed4be07f7caaa4ad3a7e268d0f5175bc71a
v_chm <vov.chm@ya.ru>
2020-02-05 02:49:22 +0500

    first and second day

--- lib/koans/12_pattern_matching.ex
+++ lib/koans/12_pattern_matching.ex
@@ -4,43 +4,43 @@ defmodule PatternMatching do
   @intro "PatternMatching"
 
   koan "One matches one" do
-    assert match?(1, ___)
+    assert match?(1, 1)
   end
 
   koan "Patterns can be used to pull things apart" do
     [head | tail] = [1, 2, 3, 4]
 
-    assert head == ___
-    assert tail == ___
+    assert head == 1
+    assert tail == [2, 3, 4]
   end
 
   koan "And then put them back together" do
     head = 1
     tail = [2, 3, 4]
 
-    assert ___ == [head | tail]
+    assert [1, 2, 3, 4] == [head | tail]
   end
 
   koan "Some values can be ignored" do
     [_first, _second, third, _fourth] = [1, 2, 3, 4]
 
-    assert third == ___
+    assert third == 3
   end
 
   koan "Strings come apart just as easily" do
     "Shopping list: " <> items = "Shopping list: eggs, milk"
 
-    assert items == ___
+    assert items == "eggs, milk"
   end
 
   koan "Maps support partial pattern matching" do
     %{make: make} = %{type: "car", year: 2016, make: "Honda", color: "black"}
 
-    assert make == ___
+    assert make == "Honda"
   end
 
   koan "Lists must match exactly" do
-    assert_raise ___, fn ->
+    assert_raise MatchError, fn ->
       [a, b] = [1, 2, 3]
     end
   end
@@ -48,7 +48,7 @@ defmodule PatternMatching do
   koan "So does the keyword lists" do
     kw_list = [type: "car", year: 2016, make: "Honda"]
     [_type | [_year | [tuple]]] = kw_list
-    assert tuple == {___, ___}
+    assert tuple == {:make, "Honda"}
   end
 
   koan "The pattern can make assertions about what it expects" do
@@ -64,9 +64,9 @@ defmodule PatternMatching do
     dog = %{type: "dog"}
     snake = %{type: "snake"}
 
-    assert make_noise(cat) == ___
-    assert make_noise(dog) == ___
-    assert make_noise(snake) == ___
+    assert make_noise(cat) == "Meow"
+    assert make_noise(dog) == "Woof"
+    assert make_noise(snake) == "Eh?"
   end
 
   koan "And they will only run the code that matches the argument" do
